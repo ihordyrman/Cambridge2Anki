150bbab316d2849986e93ad15ec60abcad93bfd5
ihordyrman <ihor.dyrman@outlook.com>
2024-10-27 21:53:22 +0200

    Try more examples + try find image

--- src/Program.fs
+++ src/Program.fs
@@ -10,33 +10,33 @@ type PartOfSpeech =
     | Noun
     | Verb
     | Adjective
+    | Adverb
+    | PhrasalVerb
 
 type Word =
     { word: string
       partOfSpeech: PartOfSpeech
       phonetic: string
       definition: string
+      image: string option
       examples: string array
       synonyms: string array }
 
 let results =
     words
-    |> fun x ->
+    |> Array.map (fun word ->
         let document =
-            BrowsingContext.New(Configuration.Default.WithDefaultLoader()).OpenAsync(cambridge x)
+            BrowsingContext.New(Configuration.Default.WithDefaultLoader()).OpenAsync(cambridge word)
             |> Async.AwaitTask
             |> Async.RunSynchronously
 
-        document.GetElementsByClassName("di-body")
-    |> Seq.toArray
-    |> Array.map (fun result ->
+        let result = document.GetElementsByClassName("di-body") |> Seq.head
 
         let definition =
             result.GetElementsByClassName "ddef_h"
             |> Seq.tryHead
-            |> Option.map (fun div -> div.TextContent |> fun def -> def.Replace(":", ""))
+            |> Option.map (fun div -> div.TextContent |> fun def -> def.Replace(":", "").Trim())
             |> Option.defaultValue ""
-
 
         let partOfSpeech =
             result.GetElementsByClassName "pos dpos"
@@ -48,6 +48,8 @@ let results =
                 | Some "noun" -> PartOfSpeech.Noun
                 | Some "verb" -> PartOfSpeech.Verb
                 | Some "adjective" -> PartOfSpeech.Adjective
+                | Some "adverb" -> PartOfSpeech.Adverb
+                | Some "phrasal verb" -> PartOfSpeech.PhrasalVerb
                 | _ -> failwith "Unknown part of speech"
 
         let phonetic =
@@ -71,11 +73,18 @@ let results =
             |> Option.map (fun x -> x.TextContent)
             |> Option.defaultValue ""
 
+        let image =
+            result.GetElementsByTagName "amp-img"
+            |> Seq.tryHead
+            |> Option.map (fun x -> "https://dictionary.cambridge.org/" + x.GetAttribute("src"))
+
         { definition = definition
           partOfSpeech = partOfSpeech
           phonetic = phonetic
           examples = examples
           synonyms = [| synonym |]
-          word = "" })
+          word = word
+          image = image })
 
-printfn $"%A{results}"
+for result in results do
+    printfn $"%A{result}"
